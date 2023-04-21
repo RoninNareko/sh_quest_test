62550d5c654457d5a2fd8c46ea69074d7cb6c2f5
narekhasratyan <hasratyan.2000@gmail.com>
2023-04-21 22:08:53 +0400

    ws fixs

--- src/components/SingleAutoComplite/SingleAutoComplete.utils.tsx
+++ src/components/SingleAutoComplite/SingleAutoComplete.utils.tsx
@@ -1,52 +1,49 @@
-import {
-  AutocompleteRenderInputParams,
-  FilterOptionsState,
-  TextField,
-  createFilterOptions,
-} from "@mui/material";
-import { DataType } from "./SingleAutoComplete.types";
+import React from "react";
+import {AutocompleteRenderInputParams, createFilterOptions, FilterOptionsState, TextField,} from "@mui/material";
+import {DataType} from "./SingleAutoComplete.types";
+
 
 export const renderOptionHandler = (
-  props: React.HTMLAttributes<HTMLLIElement>,
-  option: DataType
+    props: React.HTMLAttributes<HTMLLIElement>,
+    option: DataType
 ) => <li {...props}>{option.name}</li>;
 
 export const renderInputHandler = (params: AutocompleteRenderInputParams) => {
-  return <TextField {...params} label={"Select"} />;
+    return <TextField {...params} label={"Select"}/>;
 };
 
 export const filterOptionsHandler = (
-  options: DataType[],
-  params: FilterOptionsState<DataType>
+    options: DataType[],
+    params: FilterOptionsState<DataType>
 ) => {
-  const filter = createFilterOptions<DataType>();
-  const filtered = filter(options, params);
+    const filter = createFilterOptions<DataType>();
+    const filtered = filter(options, params);
 
-  const { inputValue } = params;
-  // Suggest the creation of a new value
-  const isExisting = options.some((option) => inputValue === option.name);
+    const {inputValue} = params;
+    // Suggest the creation of a new value
+    const isExisting = options.some((option) => inputValue === option.name);
 
-  if (inputValue !== "" && !isExisting) {
-    const addEntity = {
-      inputValue,
-      name: `Add "${inputValue}"`,
-    };
+    if (inputValue !== "" && !isExisting) {
+        const addEntity = {
+            inputValue,
+            name: `Add "${inputValue}"`,
+        };
 
-    filtered.push(addEntity);
-  }
+        filtered.push(addEntity);
+    }
 
-  return filtered.sort((a, b) => a.name.localeCompare(b.name));
+    return filtered.sort((a, b) => a.name.localeCompare(b.name));
 };
 
 export const getOptionlabelHandler = (option: DataType | string) => {
-  // Value selected with enter, right from the input
-  if (typeof option === "string") {
-    return option;
-  }
-  // Add "xxx" option created dynamically
-  if (option.inputValue) {
-    return option.inputValue;
-  }
-  // Regular option
-  return option.name;
+    // Value selected with enter, right from the input
+    if (typeof option === "string") {
+        return option;
+    }
+    // Add "xxx" option created dynamically
+    if (option.inputValue) {
+        return option.inputValue;
+    }
+    // Regular option
+    return option.name;
 };
